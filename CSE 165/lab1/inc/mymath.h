1352f4819c983424c6a888d44bc7ade1ca445abe
Jaeha Hwang <55926965+jha359@users.noreply.github.com>
2021-07-20 11:29:41 -0700

    Add files via upload

---
+++ CSE 165/lab1/inc/mymath.h
@@ -0,0 +1,23 @@
+int add(int x, int y){
+	return x+y;
+}
+
+int sub(int x, int y)
+{
+	return x - y;
+}
+
+int mult(int x, int y)
+{
+	return x * y;
+}
+
+int divide(int x, int y)
+{
+	return x / y;
+}
+
+int remain(int x, int y)
+{
+	return x % y;
+}
